f10cf59780f6d940f1ebd2c28ca7d967f7d549f7
Vaibhav <vaibhavmonu8309@gmail.com>
2023-11-27 23:07:25 +0530

    fix: verification email

--- src/components/emails/PrimaryActionEmail.tsx
+++ src/components/emails/PrimaryActionEmail.tsx
@@ -34,7 +34,7 @@ import {
         <Body style={main}>
           <Container style={container}>
             <Img
-              src={`${process.env.NEXT_PUBLIC_SERVER_URL}/hippo-newsletter-sign-up.png`}
+              src={`${process.env.NEXT_PUBLIC_SERVER_URL}/hippo-email-sent.png`}
               width='150'
               height='150'
               alt='DigitalHippo'
