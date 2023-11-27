906341d73f76ec5317d66ccf043ee9277fc2c7f6
Vaibhav <vaibhavmonu8309@gmail.com>
2023-11-27 22:15:14 +0530

    fix: image loading

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                hostname: 'localhost',
-                pathname: '**',
-                port: '3000',
-                protocol: 'http'
-            }
-        ]
+        domains: [
+            'localhost',
+            'digihippo-production.up.railway.app',
+            'dead8309.shop'
+          ],      
     }
 }
 
